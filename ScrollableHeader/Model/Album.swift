1e7f25386aea1d2070ed2df8d2472126360fbe77
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-01-13 23:12:24 +0300

    playlist items and play buttons added.

---
+++ ScrollableHeader/Model/Album.swift
@@ -0,0 +1,31 @@
+//
+//  Album.swift
+//  ScrollableHeader
+//
+//  Created by Abdullah Karaboğa on 13.01.2023.
+//
+
+import SwiftUI
+
+struct Album: Identifiable {
+    var id = UUID().uuidString
+    var albumName: String
+}
+
+var albums : [Album] = [
+Album(albumName: "Song 1"),
+Album(albumName: "Song 1"),
+Album(albumName: "Song 1"),
+Album(albumName: "Song 1"),
+Album(albumName: "Song 1"),
+Album(albumName: "Song 1"),
+Album(albumName: "Song 1"),
+Album(albumName: "Song 1"),
+Album(albumName: "Song 1"),
+Album(albumName: "Song 1"),
+Album(albumName: "Song 1"),
+Album(albumName: "Song 1"),
+Album(albumName: "Song 1"),
+    
+]
+
--- ScrollableHeader/View/Home.swift
+++ ScrollableHeader/View/Home.swift
@@ -8,52 +8,135 @@
 import SwiftUI
 
 struct Home: View {
-    var safeArea : EdgeInsets
-    var size : CGSize
+    var safeArea: EdgeInsets
+    var size: CGSize
     var body: some View {
-        ScrollView (.vertical, showsIndicators: false){
-            VStack{
+        ScrollView (.vertical, showsIndicators: false) {
+            VStack {
                 ArtWork()
-                
-                GeometryReader{ proxy in
-                    Button{
-                        
-                    } label : {
-                        Text("Play")
+
+                GeometryReader { proxy in
+                    Button {
+
+                    } label: {
+                        Text("Play Album")
                             .font(.callout)
                             .fontWeight(.semibold)
                             .foregroundColor(.white)
                             .padding(.horizontal, 45)
                             .padding(.vertical, 12)
-                            .background{
-                                Capsule()
-                                    .fill(Color("Purple").gradient)
-                            }
+                            .background {
+                            Capsule()
+                                .fill(Color("Purple").gradient)
+                        }
                     }
-                    .frame(maxWidth: .infinity,maxHeight: .infinity)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
                 }
-                .frame(height: 50)
+                    .frame(height: 50)
+                    .padding(.top, -34)
+
+                VStack {
+                    Text("Popular")
+                        .fontWeight(.heavy)
+
+                    AlbumView()
+                }
+                    .padding(.top, 10)
             }
-            
+                .overlay(alignment: .top) {
+                HeaderView()
+            }
+
         }
-        .coordinateSpace(name: "SCROLL")
+            .coordinateSpace(name: "SCROLL")
     }
-    
+
     @ViewBuilder
-    func ArtWork()-> some View{
+    func ArtWork() -> some View {
         let height = size.height * 0.4
-        
-        GeometryReader{ proxy in
+
+        GeometryReader { proxy in
             let size = proxy.size
-            
+
             Image("ArtWork")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
-                .frame(width: size.width,height: size.height)
+                .frame(width: size.width, height: size.height)
                 .clipped()
-        
+
         }
-        .frame(height: height + safeArea.top)
+            .frame(height: height + safeArea.top)
+    }
+
+    @ViewBuilder
+    func AlbumView() -> some View {
+        VStack(spacing: 25) {
+            ForEach(albums.indices, id: \.self) { index in
+
+                HStack(spacing: 25) {
+                    Text("\(index + 1)")
+                        .font(.callout)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.gray)
+
+                    VStack(alignment: .leading, spacing: 6) {
+                        Text(albums[index].albumName)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+
+                        Text("2.324.342")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+
+                    Image(systemName: "elipsis")
+                        .foregroundColor(.gray)
+                }
+
+            }
+        }
+            .padding(15)
+    }
+
+    @ViewBuilder
+    func HeaderView() -> some View {
+        GeometryReader { proxy in
+            HStack(spacing: 15) {
+                Button {
+
+                } label: {
+                    Image(systemName: "chevron.left")
+                        .font(.title3)
+                        .foregroundColor(.white)
+                }
+                Spacer(minLength: 0)
+
+                Button {
+
+                } label: {
+                    Text("Following")
+                        .font(.caption)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                        .padding(.vertical)
+                        .border(.red, width: 1.5)
+                }
+                
+                Button {
+
+                } label: {
+                    Image(systemName: "elipsis")
+                        .font(.title3)
+                        .foregroundColor(.white)
+                }
+            }
+            .padding(.top, safeArea.top + 10)
+            .padding([.horizontal, .bottom], 15)
+
+        }
+        .frame(height: 35)
     }
 }
 
